6e84275a05da034ce7440c8e686b1d1ae92174ff
Ekaterina Mikhaylova <emikhayl@cbr5s1.42barcelona.com>
2023-11-16 23:15:27 +0100

    half done

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,105 @@
 /*   By: emikhayl <emikhayl@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 21:29:13 by emikhayl          #+#    #+#             */
-/*   Updated: 2023/11/14 22:39:12 by emikhayl         ###   ########.fr       */
+/*   Updated: 2023/11/16 19:36:29 by emikhayl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUF_SIZE 6
+#define BUF_SIZE 7
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	
+	char	*joined;
+	size_t	i;
+    size_t	j;
+
+	joined = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+    j = 0;
+	if (joined == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		joined[j++] = s1[i++];
+	}
+    i = 0;
+	while (s2[i])
+	{
+		joined[j++] = s2[i++];
+	}
+	joined[j] = '\0';
+	return (joined);
+}
+
+char *append_buffer(char *buffer, char *spoon_buffer)
+{
+    char *temp;
+    
+    temp = ft_strjoin(buffer, spoon_buffer);
+    free(buffer);
+    return (temp);
+}
+
+void    print_newline_helper(char *buffer)
+{
+
+    while (*buffer)
+    {
+        if (*buffer == '\n') 
+        {
+            *buffer= '#';
+        }
+        printf("%c",*buffer);
+        buffer++;
+    }
+}
+
+char *read_line(int fd)
+{
+    int bytes_read;
+    char *spoon_buffer;
+    
+    spoon_buffer = malloc(sizeof(char) * BUF_SIZE + 1);
+    if (!spoon_buffer)
+        return NULL;
+    bytes_read = read(fd, spoon_buffer, BUF_SIZE);
+    print_newline_helper(spoon_buffer);
+    if (bytes_read <= 0)
+	{
+		free(spoon_buffer);
+        return NULL;
+	}
+    spoon_buffer[bytes_read] = '\0';
+    return spoon_buffer;
+}
 
 char *get_next_line(int fd)
 {
-    int bytes_read;
-    char *buffer;
-    static int count = 1;
-    printf("malloc#[%d]---", count++);
-    buffer = malloc(BUF_SIZE * sizeof(char) + 1);
-    if (!buffer)
-        return NULL;
-    bytes_read = read(fd, buffer, BUF_SIZE);
-    if (bytes_read <= 0)
-	{
-		free(buffer);
-        return NULL;
-	}
-	buffer[BUF_SIZE] = '\0';
-    return buffer;
+    static char *big_buffer;
+    char *line = NULL;
+    
+    big_buffer = malloc(sizeof(char) + 1);
+    line = read_line(fd);
+
+
+    big_buffer = ft_strjoin(big_buffer, line);
+    return(big_buffer);
 }
 
 int main (void)
@@ -45,7 +119,7 @@ int main (void)
 		return(1);
 	while(42)
 	{
-	next_line = get_next_line(fd);
+	next_line = read_line(fd);
 	if (next_line == NULL)
 		break;
 	count++;
--- get_next_line.h
+++ get_next_line.h
@@ -21,7 +21,7 @@
 
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
 char *get_next_line(int fd);
 void    ft_bzero(void *s, size_t n);
