e30d13716130f9c8106bb2dff4a611f765f4ba31
ekatemi <100319631+ekatemi@users.noreply.github.com>
2023-11-17 17:47:22 +0100

    test

--- test_next_line.c
+++ test_next_line.c
@@ -4,70 +4,123 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUF_SIZE 20
+#include "get_next_line.h"
+
+#define BUF_SIZE 7
+
+int ft_strlen(char *str)
+{
+    int len;
+
+    len = 0;
+    while (*str)
+    {
+        len++;
+        str++;
+    }
+    return (len);
+}
+
+char    *ft_strjoin(char *s1, char *s2)
+{
+
+    char    *joined;
+    size_t  i;
+    size_t  j;
+
+    joined = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    i = 0;
+    j = 0;
+    if (joined == NULL)
+        return (NULL);
+    while (s1[i])
+    {
+        joined[j++] = s1[i++];
+    }
+    i = 0;
+    while (s2[i])
+    {
+        joined[j++] = s2[i++];
+    }
+    joined[j] = '\0';
+    return (joined);
+}
+
+char *append_buffer(char *buffer, char *spoon_buffer)
+{
+    char *temp;
+
+    temp = ft_strjoin(buffer, spoon_buffer);
+    free(buffer);
+    return (temp);
+}
+
+void    print_newline_helper(char *buffer)
+{
+
+    while (*buffer)
+    {
+        if (*buffer == '\n')
+        {
+            *buffer= '#';
+        }
+        printf("%c",*buffer);
+        buffer++;
+    }
+}
+
+char *read_line(int fd)
+{
+    int bytes_read;
+    char *spoon_buffer;
+
+    spoon_buffer = malloc(sizeof(char) * BUF_SIZE + 1);
+    if (!spoon_buffer)
+        return NULL;
+    bytes_read = read(fd, spoon_buffer, BUF_SIZE);
+    print_newline_helper(spoon_buffer);
+    if (bytes_read <= 0)
+    {
+        free(spoon_buffer);
+        return NULL;
+    }
+    spoon_buffer[bytes_read] = '\0';
+    return spoon_buffer;
+}
 
 char *get_next_line(int fd)
 {
-    char *buffer = malloc(BUF_SIZE + 1);
-    if (!buffer)
-        return NULL;
+    static char *big_buffer;
+    char *line = NULL;
 
-    char *newline = NULL;
-    int position = 0;
+    big_buffer = malloc(sizeof(char) + 1);
+    line = read_line(fd);
 
-    while (1)
-    {
-        int bytes_read = read(fd, buffer + position, 1);
-        if (bytes_read <= 0)
-            break;
 
-        if (buffer[position] == '\n')
-        {
-            newline = strndup(buffer, position);
-            position = 0; // Reset position for the next line
-            break;
-        }
-
-        position++;
-
-        if (position == BUF_SIZE)
-        {
-            // Buffer full, need to allocate more space
-            free(buffer);
-            buffer = malloc((BUF_SIZE * 2) + 1);
-            if (!buffer)
-                return NULL;
-        }
-    }
-
-    if (position > 0)
-    {
-        // If there's content left in the buffer after the loop ends
-        newline = strndup(buffer, position);
-    }
-
-    return newline;
+    big_buffer = ft_strjoin(big_buffer, line);
+    return(big_buffer);
 }
 
-int main(void)
+int main (void)
 {
     int fd;
     char *next_line;
-    //int count = 0;
+    int count;
 
+    count = 0;
     fd = open("archivo.txt", O_RDONLY);
     if (fd == -1)
+        return(1);
+    while(42)
     {
-        return 1;
+    next_line = read_line(fd);
+    if (next_line == NULL)
+        break;
+    count++;
+    printf("[%d] :%s\n", count, next_line);
+    free(next_line);
+    next_line = NULL;
     }
-
-    while ((next_line = get_next_line(fd)) != NULL)
-    {
-        //count++;
-        printf("%s\n", next_line);
-        free(next_line);
-    }
-
     close(fd);
-    return 0;
+    return (0);
 }
